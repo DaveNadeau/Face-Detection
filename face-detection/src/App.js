bef2fefb7644b11e17d53192ded4bc90c36cbb54
Dave Nadeau <37254473+DaveNadeau@users.noreply.github.com>
2020-07-18 16:25:09 -0700

    Particle.js added

--- face-detection/src/App.js
+++ face-detection/src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank'
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Particles from 'react-particles-js';
 import "tachyons";
 import './App.css';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 120,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particleOptions}
+              />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRacognition />*/}
     </div>
