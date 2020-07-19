9c6659c48ffbbddb8e26be09edc557a93dfa79b5
Dave Nadeau <37254473+DaveNadeau@users.noreply.github.com>
2020-07-19 15:16:17 -0700

    basic input and detect button functionality added

--- face-detection/src/App.js
+++ face-detection/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank'
@@ -20,19 +20,40 @@ const particleOptions = {
 }
 
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particleOptions}
-              />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRacognition />*/}
-    </div>
-  );
+// function App()
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particleOptions}
+                />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}/>
+        {/*<FaceRacognition />*/}
+      </div>
+    );
+  }
+
 }
 
 export default App;
